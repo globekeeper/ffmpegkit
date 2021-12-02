c90962f2fe09e967c9215591d69d24d96386d381
Artem Krachulov <artem.krachulov@gmail.com>
2021-12-02 17:47:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "ffmpegkit", targets: ["ffmpegkit"])
     ],
     targets: [
-        .binaryTarget(name: "ffmpegkit", url: "https://github.com/globekeeper/ffmpegkit/releases/download/v4.5/ffmpegkit.xcframework.zip", checksum: "2b3ee55bd2098009071a3af9bc40498e7a5203612ab114c99cde87bc258731c5")
+        .binaryTarget(name: "ffmpegkit", url: "https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5/ffmpeg-kit-audio-4.5-ios-xcframework.zip", checksum: "2b3ee55bd2098009071a3af9bc40498e7a5203612ab114c99cde87bc258731c5")
     ]
 )
