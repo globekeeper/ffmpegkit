3da130a050009d7730b92d4e3ed45e530f2d0f01
Artem Krachulov <artem@globekeeper.com>
2021-12-04 20:01:55 +0200

    SMP

--- Package.swift
+++ Package.swift
@@ -5,23 +5,27 @@ import PackageDescription
 let package = Package(
     name: "ffmpegkit",
     products: [
-        .library(name: "ffmpegkit", targets: ["ffmpegkit", "Depend"]),
+        .library(name: "ffmpegkit", targets: ["Depend"]),
     ],
     targets: [
         .binaryTarget(name: "ffmpegkit",
                       url: "https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5/ffmpeg-kit-audio-4.5-ios-xcframework.zip",
                       checksum: "85d4ba0f4041308962c335f884b409240ec07aec1d4b1496dde5913fa233a907"),
-        .target(name: "Depend",
-                linkerSettings: [
-                    .linkedLibrary("z"),
-                    .linkedLibrary("bz2"),
-                    .linkedLibrary("iconv"),
-                    .linkedLibrary("c++"),
-                    .linkedFramework("AVFoundation"),
-                    .linkedFramework("AudioToolbox"),
-                    .linkedFramework("CoreMedia"),
-                    .linkedFramework("VideoToolbox")
-                ]
-               )
+        
+            .target(name: "Depend",
+                    dependencies: [
+                        .target(name: "ffmpegkit"),
+                    ],
+                    linkerSettings: [
+                        .linkedLibrary("z"),
+                        .linkedLibrary("bz2"),
+                        .linkedLibrary("iconv"),
+                        .linkedLibrary("c++"),
+                        .linkedFramework("AVFoundation"),
+                        .linkedFramework("AudioToolbox"),
+                        .linkedFramework("CoreMedia"),
+                        .linkedFramework("VideoToolbox")
+                    ]
+                   )
     ]
 )
