1ccc3b43aa54233d20157d0440b13913408c6240
Artem Krachulov <artem@globekeeper.com>
2021-12-04 18:48:56 +0200

    SMP

--- Package.swift
+++ Package.swift
@@ -5,9 +5,16 @@ import PackageDescription
 let package = Package(
     name: "ffmpegkit",
     products: [
-        .library(name: "ffmpegkit", targets: ["ffmpegkit"]),
+        .library(name: "ffmpegkit", targets: ["ffmpegkit", "Depend"]),
     ],
     targets: [
-        .binaryTarget(name: "ffmpegkit", url: "https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5/ffmpeg-kit-audio-4.5-ios-xcframework.zip", checksum: "85d4ba0f4041308962c335f884b409240ec07aec1d4b1496dde5913fa233a907")
+        .binaryTarget(name: "ffmpegkit",
+                      url: "https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5/ffmpeg-kit-audio-4.5-ios-xcframework.zip",
+                      checksum: "85d4ba0f4041308962c335f884b409240ec07aec1d4b1496dde5913fa233a907"),
+        .target(name: "Depend",
+                linkerSettings: [
+                    .linkedFramework("VideoToolbox")
+                ]
+               )
     ]
 )
---
+++ Sources/Depend/Depend.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Artem Krachulov on 04.12.2021.
+//
+
+import Foundation
